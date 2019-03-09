12b2cf4450d78d1629dcd4b08db731510e373697
Jarbas Junior <jarbasoujunior@gmail.com>
2019-03-09 14:24:25 -0300

    Complete finance tracker app

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: "user/registrations" }
+  devise_for :users, controllers: { registrations: 'user/registrations' }
   root 'welcome#index'
 
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
+
   resources :user_stocks, only: %i[create destroy]
+  resources :users, only: %i[show]
+  resources :friendships
+
   get 'my_friends', to: 'users#my_friends'
+  get 'search_friends', to: 'users#search'
+  post 'add_friend', to: 'users#add_friend'
 end
